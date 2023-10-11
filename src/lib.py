29a78657b1b1499a29fa6ddcbd861c3f74c6ada4
Arshdeep Padda <aspadda@icloud.com>
2023-10-10 20:14:36 -0700

    Added handling of negative sign
    + Added handling of minus operator being indicative of negating a number. Added a new `NEG` operator for internal use. A minus found in the expression tokens is converted to NEG if it appears to be a negating operator.
    
    A minus operator is a `NEG` operator if:
    - It is the very first token
    - The previous token is a non-number (operator) and a not a right parenthesis.
    
    The `NEG` operator is not intended to passed in via the expression. It is only for internal use.
    
    + The parentheses are also added to the `operators` store.
    
    + Added `evaluate(expression)` which conducts all necessary steps to solve an expression.

--- src/lib.py
+++ src/lib.py
@@ -11,26 +11,44 @@ class Associativity(Enum):
     BOTH = 3
 
 
+class Arity:
+    NONE = 0
+    UNARY = 1
+    BINARY = 2
+
+
+def nop():
+    """ no operation """
+    return
+
+
+def neg(a):
+    """ return negative of `a` """
+    return -a
+
+
 class Operator:
-    def __init__(self, name: str, associativity: Associativity, precedence: int, operation: Callable):
+    def __init__(self, name: str, associativity: Associativity, precedence: int,
+                 operation: Callable, arity=Arity.BINARY):
         self.name = name
         self.associativity = associativity
         self.precedence = precedence
         self.operation = operation
+        self.arity = arity
 
 
 operators = {
-    "+": Operator("+", Associativity.LEFT, 2, operator.add),
-    "-": Operator("-", Associativity.LEFT, 2, operator.sub),
-    "*": Operator("*", Associativity.LEFT, 3, operator.mul),
-    "/": Operator("/", Associativity.LEFT, 3, operator.truediv),
-    "^": Operator("^", Associativity.RIGHT, 4, operator.pow)
+    "+": Operator("+", Associativity.LEFT, 1, operator.add),
+    "-": Operator("-", Associativity.LEFT, 1, operator.sub),
+    "*": Operator("*", Associativity.LEFT, 2, operator.mul),
+    "/": Operator("/", Associativity.LEFT, 2, operator.truediv),
+    "^": Operator("^", Associativity.RIGHT, 3, operator.pow),
+    "(": Operator("(", Associativity.NONE, 0, nop, arity=Arity.NONE),
+    ")": Operator(")", Associativity.NONE, 0, nop, arity=Arity.NONE),
+    "NEG": Operator("NEG", Associativity.RIGHT, 2, neg, arity=Arity.UNARY)
 }
 
-rep_split_expr = "[0-9]+[.]?[0-9]*"
-for key in operators.keys():
-    rep_split_expr += f"|[\\{key}]"
-rep_split_expr += "|[(|)]"
+rep_split_expr = "[\\d+.?\\d*]|[+|\\-|*|/|(|)|^]"
 
 
 def tokenize(expression):
@@ -45,6 +63,17 @@ def tokenize(expression):
         tokens[i:i + 1] = sub_tokens
         i += len(sub_tokens)
 
+    return tokens
+
+
+def handle_neg(tokens):
+    """" convert minus operator in tokens that should be unary negative """
+    for i, token in enumerate(tokens):
+        if token == '-':
+            if i == 0:
+                tokens[i] = 'NEG'
+            elif type(tokens[i - 1]) == str and tokens[i - 1] != ")":
+                tokens[i] = 'NEG'
     return tokens
 
 
@@ -68,11 +97,14 @@ def infix_to_postfix(tokens):
             else:
                 raise Exception("ParenthesesMismatch")
         else:
-            while (operator_stack and operator_stack[-1] != "(") \
-                    and (operators[operator_stack[-1]].precedence > operators[token].precedence or
-                         (operators[operator_stack[-1]].precedence == operators[token].precedence and
-                          operators[token].associativity == Associativity.LEFT)):
+            curOp = operators[token]
+            stkOp = operators[operator_stack[-1]] if operator_stack else None
+            while (operator_stack and operator_stack[-1] != "(") and \
+                    (stkOp.precedence > curOp.precedence or
+                     (stkOp.precedence == curOp.precedence and
+                      curOp.associativity == Associativity.LEFT)):
                 output.append(operator_stack.pop())
+                stkOp = operators[operator_stack[-1]] if operator_stack else None
             operator_stack.append(token)
 
     while operator_stack:
@@ -88,9 +120,31 @@ def postfix_solver(tokens):
         if type(token) == int:
             operands.append(token)
         else:
-            if len(operands) < 2:
-                raise Exception("BinOp_TooFewOperands")
-            operand2 = operands.pop()
-            operand1 = operands.pop()
-            operands.append(operators[token].operation(operand1, operand2))
+            if operators[token].arity == Arity.BINARY:
+                if len(operands) < 2:
+                    raise Exception("BinOp_TooFewOperands")
+                operand2 = operands.pop()
+                operand1 = operands.pop()
+                operands.append(operators[token].operation(operand1, operand2))
+            else:
+                if not operands:
+                    raise Exception("UnaryOp_TooFewOperands")
+                operands.append(operators[token].operation(operands.pop()))
     return operands.pop()
+
+
+def evaluate(expression, verbose=False):
+    tokens = tokenize(expression)
+    negatedTokens = handle_neg(tokens)
+    rpn = infix_to_postfix(negatedTokens)
+    result = postfix_solver(rpn)
+
+    if verbose:
+        x = "{: >15}"
+        print(x.format("Expression:"), expression)
+        print(x.format("Initial Tokens:"), tokens)
+        print(x.format("Final Tokens:"), negatedTokens)
+        print(x.format("Postfix:"), rpn)
+        print(x.format("Result :"), result)
+
+    return result
